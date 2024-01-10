6592171bc2bfe8b5b6a9f275fd4107dc3646c191
DIPAYANPOREL <dipayanporelcllg@gmail.com>
2024-01-10 16:12:55 +0530

    (10/Jan/2024)

---
+++ src/utils/common.js
@@ -0,0 +1,18 @@
+export const sliderSettings = {
+    sildesPerview: 1,
+    spaceBetween: 50,
+    breakpoints: {
+        480: {
+            sildesPerview: 1
+        },
+        600: {
+            sildesPerview: 2
+        },
+        750: {
+            sildesPerview: 3
+        },
+        1050: {
+            sildesPerview: 4
+        }
+    }
+}
